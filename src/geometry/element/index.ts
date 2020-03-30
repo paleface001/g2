6c7bf50fa488728a06317c3015210626731a536c
simaQ <sima.zhang1990@gmail.com>
2020-03-30 13:35:52 +0800

    refactor: 为了性能，修改部分代码写法

--- src/geometry/element/index.ts
+++ src/geometry/element/index.ts
@@ -108,8 +108,10 @@ export default class Element extends Base {
     // step 2: 使用虚拟 Group 重新绘制 shape，然后更新当前 shape
     const offscreenGroup = this.getOffscreenGroup();
     const newShape = shapeFactory.drawShape(this.shapeType, drawCfg, offscreenGroup);
-    newShape.set('data', this.data);
-    newShape.set('origin', drawCfg);
+    // @ts-ignore
+    newShape.cfg.data = this.data;
+    // @ts-ignore
+    newShape.cfg.origin = drawCfg;
 
     // step 3: 同步 shape 样式
     this.syncShapeStyle(shape, newShape, '', this.getAnimateCfg('update'));
@@ -324,19 +326,19 @@ export default class Element extends Base {
 
   // 从主题中获取对应状态量的样式
   private getStateStyle(stateName: string, shapeKey?: string): StateCfg {
-    const { theme, shapeFactory } = this;
-    let shapeType = this.shapeType;
-    // 如果用户自定义 shape，则使用默认 shape 的配置
-    if (!theme[shapeType]) {
-      shapeType = shapeFactory.defaultShapeType;
-    }
-    // 用户通过 geometry.state() 接口定义了状态样式
-    const stateOption = get(this.geometry.stateOption, stateName, {});
-    const stateCfg = deepMix({}, get(theme, [shapeType, stateName], {}), stateOption);
+    const { theme, shapeFactory, shapeType } = this;
+    const defaultState = theme[shapeType] || theme[shapeFactory.defaultShapeType];
 
-    let shapeStyle = get(stateCfg.style, [shapeKey]) ?
-      get(stateCfg.style, [shapeKey]) :
-      stateCfg.style;
+    let stateCfg;
+    const stateOption = get(this.geometry.stateOption, stateName, {});
+    if (stateOption) {
+      // 用户通过 geometry.state() 接口定义了状态样式
+      stateCfg = deepMix({}, get(defaultState, [stateName], {}), stateOption);
+    } else {
+      stateCfg = get(defaultState, [stateName], {});
+    }
+
+    let shapeStyle = get(stateCfg.style, [shapeKey], stateCfg.style);
 
     if (isFunction(shapeStyle)) {
       shapeStyle = shapeStyle(this);
@@ -368,9 +370,11 @@ export default class Element extends Base {
       if (!this.shape.get('name')) {
         // TODO: 当用户设置了 name 后，为了保证 geometry:eventName 这样的事件能够正常触发，需要加一个 inheritName
         // 等 G 事件改造完成后加上
-        this.shape.set('name', this.shapeFactory.geometryType);
+        // @ts-ignore
+        this.shape.cfg.name = this.shapeFactory.geometryType
       }
-      this.shape.set('inheritNames', ['element']);
+      // @ts-ignore
+      this.shape.cfg.inheritName = [ 'element' ];
       // 执行入场动画
       const animateType = isUpdate ? 'enter' : 'appear';
       const animateCfg = this.getAnimateCfg(animateType);
@@ -397,8 +401,10 @@ export default class Element extends Base {
 
   // 设置 shape 上需要携带的信息
   private setShapeInfo(shape: IShape | IGroup, data: ShapeInfo) {
-    shape.set('origin', data);
-    shape.set('element', this); // 考虑是否可以使用 G 事件的 delegateObject
+    // @ts-ignore
+    shape.cfg.origin = data;
+    // @ts-ignore
+    shape.cfg.element = this;
     if (shape.isGroup()) {
       const children = shape.get('children');
       children.forEach((child) => {
